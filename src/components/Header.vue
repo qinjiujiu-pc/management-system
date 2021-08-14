fbd6bbc992b3d280f34b1d1172a2b4ef9a74bcbd
516286117 <516286117@qq.com>
2021-08-14 08:54:47 +0800

    8.14日的提交

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,9 +9,35 @@
 </template>
 
 <script>
+import { reactive, toRefs } from 'vue'
+import { useRouter } from 'vue-router'
 export default {
-  name: 'Header'
+  name: 'Header',
+  setup() {
+    // 获取路由实例
+    const router = useRouter()
+    // 声明路由和 title 对应的键值对
+    const pathMap = {
+      index: '首页',
+      add: '添加商品'
+    }
+    const state = reactive({
+      name: '首页'
+    })
+    // 监听路由变化方法 afterEach
+    router.afterEach((to) => {
+      console.log('to', to)
+      // to 能获取到路由相关信息。
+      const { id } = to.query
+      state.name = pathMap[to.name]
+    })
+
+    return {
+      ...toRefs(state)
+    }
+  }
 }
+
 </script>
 
 <style scoped>
